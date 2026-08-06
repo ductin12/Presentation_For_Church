e213ba1cf05f1db06dd1fb9308dc45361a381312
Hanaweb.vn <49180047+ductin12@users.noreply.github.com>
2026-08-06 22:47:01 +0700

    feat: replace old banners with new custom banners

--- web/src/components/Hero.tsx
+++ web/src/components/Hero.tsx
@@ -10,9 +10,8 @@ interface HeroProps {
 
 const mediaItems = [
   { type: "video", src: "/Presentation-for-church-app.mp4" },
-  { type: "image", src: "/hero-1.png" },
-  { type: "image", src: "/hero-2.png" },
-  { type: "image", src: "/hero-3.png" },
+  { type: "image", src: "/Presentation-for-church-app-free.png" },
+  { type: "image", src: "/ung-dung-trinh-chieu-tho-phuong-mien-phi.png" },
 ];
 
 export default function Hero({ version }: HeroProps) {
